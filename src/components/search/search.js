5804af6c27da91ceab5631b52110133f56bfabbc
Ruslan <texs14@yandex.ru>
2020-09-15 21:00:14 +0300

    исправлена ошибка со страницами

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { search } from '../../actions/';
+import { search, changetPage } from '../../actions/';
 
 import './search.scss';
 
-const Search = ({search}) => {
+const Search = ({search, changetPage}) => {
     
     let request;
     
     return (
-        <form className='search' onSubmit={e => {e.preventDefault(); search(request.value)}}>
+        <form className='search' onSubmit={e => {e.preventDefault(); search(request.value); changetPage(0)}}>
             <input type='text' placeholder='What you are looking for...' ref={ref => request = ref}/>
             <button>Search</button>
         </form>
@@ -18,7 +18,8 @@ const Search = ({search}) => {
 }
 
 const mapDispatchToProps = {
-    search
+    search,
+    changetPage
 }
 
 export default connect(null, mapDispatchToProps)(Search);
